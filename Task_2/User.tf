38c93ed949a196947194f23093d996da7fb4459d
Ahmed Aboagye <ahmeddevops123@gmail.com>
2020-11-15 22:59:55 +0000

    README.md

---
+++ Task_2/User.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_user" "Terraform-Resource-User-Created-Task2_Ron" {
+  name = "Ron"
+  path = "/"
+}
+
+resource "aws_iam_user" "Terraform-Resource-User-Created-Task2_Sam" {
+  name = "Sam"
+  path = "/"
+}
+
+resource "aws_iam_user" "Terraform-Resource-User-Created-Task2_Billy" {
+  name = "Billy"
+  path = "/"
+}
