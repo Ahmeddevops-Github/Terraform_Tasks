4bf285bfa562846c219f23d28d51d0f621ebcc55
Ahmed Aboagye <ahmeddevops123@gmail.com>
2020-11-15 21:42:46 +0000

    User  Added

---
+++ Task_1/User.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_user" "Terraform-Resource-User-Created-Task1_Bob" {
+  name = "Bob"
+  path = "/"
+}
+
+resource "aws_iam_user" "Terraform-Resource-User-Created-Task1_Tim" {
+  name = "Tim"
+  path = "/"
+}
+
+resource "aws_iam_user" "Terraform-Resource-User-Created-Task1_Ben" {
+  name = "Ben"
+  path = "/"
+}
