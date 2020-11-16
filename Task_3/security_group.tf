41f9d0c6a0740d66467478cee6e7a2b19b7c52cc
Ahmed Aboagye <ahmeddevops123@gmail.com>
2020-11-16 06:02:37 +0000

    Security Group

---
+++ Task_3/security_group.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "Terraform-Task_3-Security-Group" {
+  name        = "Task_3-Security-Group"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "SSH - TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS - TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP - TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Telnet - TLS from VPC"
+    from_port   = 23
+    to_port     = 23
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SMTP/Mail - TLS from VPC"
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
