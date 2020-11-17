7271668db1cd613f56d11bade51c4f078c35a47a
Ahmed Aboagye <ahmeddevops123@gmail.com>
2020-11-17 12:01:46 +0000

    Admin Policy

--- Task_4/Admin_Policy.tf
+++ Task_4/Admin_Policy.tf
@@ -20,3 +20,4 @@ resource "aws_iam_role_policy_attachment" "Terraform-Created-Centos_IAM_Admin_Po
   role       = "${aws_iam_role.Terraform-Created-Centos_IAM_Admin_Role.name}"
   policy_arn = "${aws_iam_policy.Terraform-Created-Centos_IAM_Admin_Policy.arn}"
 }
+
