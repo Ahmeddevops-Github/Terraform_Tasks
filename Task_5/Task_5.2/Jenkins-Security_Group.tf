e2b642fa8356b3196a2948eeb1c774bf0ea19528
Ahmed Aboagye <ahmeddevops123@gmail.com>
2020-11-19 21:40:56 +0000

    Security Group

---
+++ Task_5/Task_5.2/Jenkins-Security_Group.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "Terraform-Jenkins-Security_Group" {
+  name = "Jenkins Security Group"
+
+  ingress {
+    description = "HTTPS - TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP - TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH - TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP Alternate - TLS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
