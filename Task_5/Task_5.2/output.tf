fe9b4336955df34d1e30dd6c265cb46e34a26215
Ahmed Aboagye <ahmeddevops123@gmail.com>
2020-11-19 22:24:26 +0000

    Output

--- Task_5/Task_5.2/output.tf
+++ Task_5/Task_5.2/output.tf
@@ -5,3 +5,15 @@ output "Terraform-Jenkins_Instance" {
 output "DNS" {
   value = "${aws_route53_record.www.name}"
 }
+
+# output "Jenkins AMI" {
+#   value = "${data.aws_ami.Terraform-Jenkins_Instance.id}"
+# }
+
+# output "Jenkins Admin" {
+#   value = "Ahmed_DevOps"
+# }
+
+# output "Jenkins Password" {
+#   value = "Redhat"
+# }
