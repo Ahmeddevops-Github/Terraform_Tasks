5aff0ecf7cc87c8e42f2f4ef30fa9f32729f2c37
Ahmed Aboagye <ahmeddevops123@gmail.com>
2020-11-20 10:09:49 +0000

    Added Security Group

---
+++ Task_5/Task_5.3/Nagios-Security_Group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "Terraform-Nagios-Security_Group" {
+  name = "Nagios Security Group"
+
+  ingress {
+    description = "HTTP - TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH - TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
